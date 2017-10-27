b4d52f5dcecc2af3532eaac579f0a7533fb6b00c
ryanalco <RyanAlco@ryans-macbook-pro.local>
2017-10-27 12:32:02 -0700

    Initial Commit

---
+++ Final Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Final Project
+//
+//  Created by Ryan Alvarez-Cohen on 10/27/17.
+//  Copyright © 2017 Ryan Alvarez-Cohen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
